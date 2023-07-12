7c834a5e86442d8305bba502dd859e44de37d726
jobscisco <139334915+jobscisco@users.noreply.github.com>
2023-07-12 17:14:31 +0800

    Update streamlit_app.py
    Added menu options

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,5 +2,9 @@ import streamlit
 
 streamlit.title('My Parents New Healthy Diner')
 
+streamlit.header('Breakfast Menu')
+streamlit.text('Omega 3 & Blueberry Outmeal')
+streamlit.text('Kale, Spinach & Rocket Smoothie')
+streamlit.text('Hard-boiled Free-Range Egg')
 
 
